b6f65bc3328218b97706dc28a4372a59e6ebc113
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2022-03-17 19:43:44 +0800

    Initial Commit

---
+++ connectFour/connectFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectFourApp.swift
+//  connectFour
+//
+//  Created by User06 on 2022/3/17.
+//
+
+import SwiftUI
+
+@main
+struct connectFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
